e44b2460f462eb8558868abb38f8a7ab37e62b7c
CodyAnagnost <codyanagnost@hotmail.com>
2017-01-31 14:07:00 -0500

    Burma_Shave.cs

--- CS_Chapter1/BurmaShave/Program.cs
+++ CS_Chapter1/BurmaShave/Program.cs
@@ -10,6 +10,15 @@ namespace BurmaShave
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("You can");
+            Console.WriteLine("Drive");
+            Console.WriteLine("A mile");
+            Console.WriteLine("A minute");
+            Console.WriteLine("But there");
+            Console.WriteLine("Is no");
+            Console.WriteLine("Future");
+            Console.WriteLine("In it");
+            Console.WriteLine("Burma Shave");
         }
     }
 }
